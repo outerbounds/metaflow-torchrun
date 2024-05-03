06a78d0d5604dcd87d6e20c169f8e729d68404f7
Eddie Mattia <eddie@outerbounds.com>
2024-05-03 11:40:29 -0700

    add long_description

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     description="An EXPERIMENTAL torchrun decorator for Metaflow",
     author="Eddie Mattia",
     author_email="eddie@outerbounds.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_namespace_packages(include=["metaflow_extensions.*"]),
     py_modules=[
         "metaflow_extensions",
