81305345df1177e265361192a48aa5a6918b817b
Madhur Tandon <madhur@outerbounds.co>
2024-12-28 19:49:40 +0530

    fix comment

--- examples/monitor-test/flow.py
+++ examples/monitor-test/flow.py
@@ -18,10 +18,7 @@ class HelloTorchrun(FlowSpec):
         image="registry.hub.docker.com/pytorch/pytorch:2.5.1-cuda12.4-cudnn9-runtime",
         cpu=2
     )
-    # to remove the following warning, but not strictly required:
-    ## Setting OMP_NUM_THREADS environment variable for each process to be 1 in default,
-    ## to avoid your system being overloaded, please further tune the variable for optimal
-    ## performance in your application as needed.
+    # we set this environment variable to remove a warning from the console. It is not strictly required.
     @environment(vars={"OMP_NUM_THREADS": 1})
     @torchrun
     @step
